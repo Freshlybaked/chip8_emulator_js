6351a1de2387daab3890d7b6df3e6bb09b6ff482
Sam <sambarnard90@gmail.com>
2025-07-07 18:25:28 +0800

    Removed requestanimationframe and moved to setinterval - RAF was too slow
    Deleted unneeded stuff for now

--- src/ClockTimer.ts
+++ src/ClockTimer.ts
@@ -1,16 +1,9 @@
 export class ClockTimer{
     public running:boolean = false;
-    public frameCount:number;
-    public fps:number;
     private fpsInterval:number;
-    public startTime:DOMHighResTimeStamp;
-    public then:DOMHighResTimeStamp;
-    public elapsed:DOMHighResTimeStamp;
-    public now:DOMHighResTimeStamp;
 
     constructor(fps:number) {
         this.fpsInterval = 1000 / fps;
-        console.log(`fpsInterval: ${this.fpsInterval}`);
     }
 
     private tickCallback:(() => void) | undefined;
@@ -26,10 +19,7 @@ export class ClockTimer{
         }
 
         this.running = true;
-        this.then = window.performance.now();
-        this.startTime = this.then;
-
-        this.tick();
+        window.setInterval(this.tick, 0);
     }
 
     stop(){
@@ -43,24 +33,8 @@ export class ClockTimer{
             return;
         }
 
-        requestAnimationFrame(this.tick);
-        
-        this.now = window.performance.now();
-        this.elapsed = this.now - this.then;
-        if (this.elapsed > this.fpsInterval) {
-            this.then = this.now - (this.elapsed % this.fpsInterval);
-
-            if(this.tickCallback != null){
-                this.tickCallback();
-            }
-
-            // this.logFps();
+        if(this.tickCallback != null){
+            this.tickCallback();
         }
-    }
-    
-    logFps(){
-        var sinceStart = this.now - this.startTime;
-        var currentFps = Math.round(1000 / (sinceStart / ++this.frameCount) * 100) / 100;
-        console.log("Elapsed time= " + Math.round(sinceStart / 1000 * 100) / 100 + " secs @ " + currentFps + " fps.");
     }
 }
